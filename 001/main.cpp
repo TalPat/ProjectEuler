343a790031bf963d2977e170aba8153092472345
TalPat <talonpatterson94@gmail.com>
2025-07-30 14:02:53 +0200

    PE 003

--- 001/main.cpp
+++ 001/main.cpp
@@ -4,18 +4,23 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Is an argument provided
-    if ( argc < 2) {
+    if (argc < 2)
+    {
         cerr << "Provide a positive integer" << endl;
+        return 1;
     }
 
     int target = stoi(argv[1]);
     // vector<int> factors;
     int total = 0;
 
-    for(int i = 0; i < target; i++) {
-        if (!(i % 3) || !(i%5)) {
+    for (int i = 0; i < target; i++)
+    {
+        if (!(i % 3) || !(i % 5))
+        {
             // factors.push_back(i);
             total += i;
         }
---
+++ 003/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        return 1;
+    }
+
+    unsigned long long input = stoull(argv[1]);
+    int largestPrime = 1;
+
+    // edgecase input is 1
+    if (input == 1)
+    {
+        cout << largestPrime << endl;
+        return 0;
+    }
+
+    while (!(input % 2))
+    {
+        largestPrime = 2;
+        input /= 2;
+        cout << largestPrime << endl;
+    }
+
+    for (int i = 3; i <= sqrt(input); i += 2){
+        if (!(input%i)){
+            input /= i;
+            largestPrime = i;
+            cout << largestPrime << endl;
+        }
+        // cout << i << "  "<< sqrt(input)<<endl;
+    }
+
+    if (input > 2) {
+        largestPrime = input;
+    }
+
+    cout << largestPrime << endl;
+    return 0;
+}
