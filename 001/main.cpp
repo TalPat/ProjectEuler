6c53edbf3aae0ef636184657ebf58fdfb54036fd
TalPat <talonpatterson94@gmail.com>
2025-07-30 12:14:16 +0200

    001 cpp

---
+++ 001/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    // Is an argument provided
+    if ( argc < 2) {
+        cerr << "Provide a positive integer" << endl;
+    }
+
+    int target = stoi(argv[1]);
+    // vector<int> factors;
+    int total = 0;
+
+    for(int i = 0; i < target; i++) {
+        if (!(i % 3) || !(i%5)) {
+            // factors.push_back(i);
+            total += i;
+        }
+    }
+
+    cout << total << endl;
+    return 0;
+}
